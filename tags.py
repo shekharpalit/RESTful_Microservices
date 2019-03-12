5b736403b842a16eb6e24a62a71153eec5211a79
rohanbedarkar1 <45887786+rohanbedarkar1@users.noreply.github.com>
2019-03-11 21:53:30 -0700

    Update tags.py
    completed all services of tags 
    Add tags to an existing URL
    Retrieve the tags for an individual URL (articles)(refer query)
    Retrieve a list of URLs(articles) with a given tag DONE

--- tags.py
+++ tags.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 
 
 @app.route('/tags',methods = ['GET'])
-
 def getArticlesFromTag():
     if request.method == 'GET':
         data = request.args.get('tag')
@@ -19,14 +18,24 @@ def getArticlesFromTag():
             tmod= datetime.now()
             cur.execute("Select * from article where art_id IN(Select art_id from normTable where tag_ID in (Select tag_id from tags WHERE tag_name =:tag_name ))", {"tag_name":data})
             row = cur.fetchall()
+            return jsonify(row)
+
+#get tags from the url utility
+@app.route('/tags/<string:art_id>',methods = ['GET'])
+def getTagsFromArticle(art_id):
+    if request.method == 'GET':
+        with sqlite3.connect('tags.db') as conn:
+            cur = conn.cursor()
+            cur.execute("SELECT tag_name from tags WHERE tag_ID IN (SELECT tag_ID from normTable WHERE art_id=:art_id )", {"art_id":art_id})
+            row = cur.fetchall()
             print(row)
             return jsonify(row)
+
 
 @app.route('/tags', methods = ['POST'])
 def tags():
     if request.method == 'POST':
         data = request.get_json(force=True)
-
         with sqlite3.connect('tags.db') as conn:
             cur = conn.cursor()
             #check if tag exists or not
@@ -36,8 +45,30 @@ def tags():
             return "tags inserted successfully \n"
 
 
-@app.route('/tags', methods = ['DELETE'])
+#adding a new and existing tag to the article
+@app.route('/tags', methods = ['PUT'])
+def addTagsToExistingArticle():
+    if request.method == 'PUT':
+        data = request.get_json(force=True)
+        tags =data['tags']
+        for tag in tags:
+                with sqlite3.connect('tags.db') as conn:
+                    cur = conn.cursor()
+                    cur.execute("SELECT tag_ID FROM tags WHERE tag_name=:tag_name",{"tag_name":tag})
+                    result = cur.fetchone()
+                    if str(result)!="None":
+                        tag_id =str(result[0])
+                        #add the new tag here
+                        #insert the relation if not exists
+                        cur.execute("INSERT INTO normTable(tag_ID, art_id) SELECT (:tag_id),(:art_id) WHERE NOT EXISTS(SELECT 1 FROM normTable WHERE tag_ID= :tag_id  AND art_id = :art_id)", {"tag_id":tag_id, "art_id":data['art_id']})
+                    elif str(result)=="None":
+                        cur.execute("INSERT INTO tags(tag_name) VALUES(:tag_name)",{"tag_name":tag})
+                        new_tag_inserted_id =cur.lastrowid
+                        cur.execute("INSERT INTO normTable(tag_ID, art_id)VALUES(:tag_ID, :art_id)",{"tag_ID":new_tag_inserted_id,"art_id":data['art_id']})
+    return "tags added to the article", 201
 
+
+@app.route('/tags', methods = ['DELETE'])
 def deleteTagFromArticle():
     if request.method == 'DELETE':
         tag_name = request.args.get('tag_name')
@@ -53,4 +84,4 @@ def deleteTagFromArticle():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
