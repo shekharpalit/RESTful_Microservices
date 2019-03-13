ee8e43c727dde0850721aabce527e0b687e120e2
akshaybhor <47091506+akshaybhor@users.noreply.github.com>
2019-03-13 14:56:47 -0700

    Try catch block added

--- article.py
+++ article.py
@@ -24,15 +24,27 @@ def tcreate():
 def insertarticle():
     if request.method == 'POST':
         data = request.get_json(force = True)
-        with sqlite3.connect('art.db') as conn:
-            cur = conn.cursor()
-            current_time= datetime.datetime.now()
-            isActiveArticle=1
-            cur.execute("INSERT INTO article(title,author,date_created,date_modified,isActiveArticle,url) VALUES (:title,:author,:date_created,:date_modified,:isActiveArticle,:URL)",{"title":data['title'],"author":data['author'],"date_created":current_time,"date_modified":current_time,"isActiveArticle":isActiveArticle,"URL":data['URL']})
-            author_ID = cur.lastrowid
-            url_article=("http://127.0.0.1:5000/article/"+str(author_ID)+"")
-            cur.execute("UPDATE article set URL=? where article_id=?",(url_article,author_ID))
-    return "DAtA inserted"
+        executionState:bool = False
+        try:
+
+            with sqlite3.connect('art.db') as conn:
+                cur = conn.cursor()
+                current_time= datetime.datetime.now()
+                isActiveArticle=1
+                cur.execute("INSERT INTO article(title,author,date_created,date_modified,isActiveArticle,url) VALUES (:title,:author,:date_created,:date_modified,:isActiveArticle,:URL)",{"title":data['title'],"author":data['author'],"date_created":current_time,"date_modified":current_time,"isActiveArticle":isActiveArticle,"URL":data['URL']})
+                author_ID = cur.lastrowid
+                url_article=("http://127.0.0.1:5000/article/"+str(author_ID)+"")
+                cur.execute("UPDATE article set URL=? where article_id=?",(url_article,author_ID))
+                if(cur.rowcount >=1):
+                    executionState = True
+                    conn.commit()
+        except:
+            print(Error)
+        finally:
+            if executionState:
+                return jsonify(message="Data Instersted Sucessfully"), 200
+            else:
+                return jsonify(message="Failed to insert data"), 409
 
 
 #cur.execute("UPDATE post_article set art=?,lmod_time=? where id=?", (data['art'],tmod,data['id']))
@@ -92,26 +104,50 @@ def Article():
 @app.route('/article',methods = ['PATCH'])
 def updateArticle():
     if request.method == 'PATCH':
-        data = request.get_json(force = True)
-        with sqlite3.connect('art.db') as conn:
-            cur = conn.cursor()
-            tmod= datetime.datetime.now()
-            cur.execute("UPDATE article set content=?,date_modified=? where article_id=?", (data['content'],tmod,data['article_id']))
-    return "Rows Updated"
-
+        executionState:bool = False
+        try:
+            data = request.get_json(force = True)
+            with sqlite3.connect('art.db') as conn:
+                cur = conn.cursor()
+                tmod= datetime.datetime.now()
+                cur.execute("UPDATE article set content=?,date_modified=? where article_id=?", (data['content'],tmod,data['article_id']))
+                if(cur.rowcount >=1):
+                    executionState = True
+                    conn.commit()
+        except:
+            conn.rollback()
+            print("Error in update")
+        finally:
+            if executionState:
+                return jsonify(message="Updated Article SucessFully"), 200
+            else:
+                return jsonify(message="Failed to update Article"), 409
 
 #delete article by article id
 
 @app.route('/article', methods = ['DELETE'])
 def deleteArticle():
     if request.method == 'DELETE':
-        article_id = request.args.get('article_id')
-        with sqlite3.connect('art.db') as conn:
-            cur = conn.cursor()
-            cur.execute("delete from article where article_id=:article_id",{"article_id":article_id})
-            row = cur.fetchall()
-            conn.commit()
-    return "Article Deleted"
+        try:
+            article_id = request.args.get('article_id')
+            executionState:bool = False
+            with sqlite3.connect('art.db') as conn:
+                cur = conn.cursor()
+                cur.execute("delete from article where article_id=:article_id",{"article_id":article_id})
+                row = cur.fetchall()
+                if cur.rowcount >= 1:
+                    executionState = True
+                conn.commit()
+
+        except sqlite3.Error as er:
+            conn.rollback()
+            print("Error:")
+            print(er.message)
+        finally:
+            if executionState:
+                return jsonify(message="Deleted Article SucessFully"),200
+            else:
+                return jsonify(message="Failed to delete Article"),409
 
 
 
