be3d2c48ea1eed3210d82a763036c829caf29e0b
rohanbedarkar1 <rohan_bedarkar@csu.fullerton.edu>
2019-03-12 21:09:45 -0700

    Updated the execution order of script

--- database.py
+++ database.py
@@ -8,12 +8,13 @@ conn = sqlite3.connect('./Main.db', timeout=10)
 print("Successfully connected")
 
 conn.execute('CREATE TABLE if not exists users (user_name TEXT PRIMARY KEY NOT NULL,  hashed_password TEXT NOT NULL, full_name TEXT NOT NULL, email_id TEXT NOT NULL,  date_created DATE NOT NULL, is_active INTEGER NOT NULL)')
-#conn.execute('CREATE TABLE if not exists article (article_id INTEGER PRIMARY KEY NOT NULL, article_title TEXT, author INTEGER NOT NULL, content TEXT NOT NULL, isActiveArticle TEXT, date_created INTEGER, date_modified INTEGER, FOREIGN KEY (author) REFERENCES users(user_name))')
-#conn.execute('CREATE TABLE if not exists tags (tag_id INTEGER PRIMARY KEY NOT NULL, tag_name TEXT)')
-##conn.execute('CREATE TABLE if not exists tag_article_mapping (tag_id INTEGER, article_id INTEGER, FOREIGN KEY (article_id) REFERENCES article(article_id), FOREIGN KEY (tag_id) REFERENCES tags(tag_id))')
+conn.execute('CREATE TABLE if not exists article (article_id INTEGER PRIMARY KEY NOT NULL, article_title TEXT, author INTEGER NOT NULL, content TEXT NOT NULL, isActiveArticle TEXT, date_created INTEGER, date_modified INTEGER, FOREIGN KEY (author) REFERENCES users(user_name))')
+conn.execute('CREATE TABLE if not exists tags (tag_id INTEGER PRIMARY KEY NOT NULL, tag_name TEXT)')
+conn.execute('CREATE TABLE if not exists tag_article_mapping (tag_id INTEGER, article_id INTEGER, FOREIGN KEY (article_id) REFERENCES article(article_id), FOREIGN KEY (tag_id) REFERENCES tags(tag_id))')
+conn.execute('CREATE TABLE if not exists comments (comment_id INTEGER PRIMARY KEY, comment TEXT, article_id INTEGER, tag_id INTEGER, timestamp INTEGER, FOREIGN KEY (article_id) REFERENCES article(article_id), FOREIGN KEY (tag_id) REFERENCES tags(tag_ID)')
 print ("Table created successfully")
 conn.close()
-#CREATE TABLE if not exists comments (comment_id INTEGER PRIMARY KEY, comment TEXT, article_id INTEGER, tag_id INTEGER, timestamp INTEGER, FOREIGN KEY (article_id) REFERENCES article(article_id), FOREIGN KEY (tag_id) REFERENCES tags(tag_ID))
+
 #old_ database queries is as follows
 '''
 conn.execute('CREATE TABLE if not exists users (user_id INTEGER PRIMARY KEY NOT NULL, user_name TEXT NOT NULL,  hash_pwd TEXT NOT NULL, name TEXT NOT NULL, email_id TEXT NOT NULL,  date_created DATE NOT NULL, is_active INTEGER NOT NULL)')
