999b9313c96a03bfd3ce3a3746d08ea61df63626
Shekhar Palit <shekharpalit222@gmail.com>
2019-03-12 23:22:52 -0700

    added authentication to user microservice

--- user.py
+++ user.py
@@ -1,71 +1,12 @@
-import os
-from flask import Flask, request, jsonify, make_response, g, current_app, Response
+from flask import Flask, request, jsonify, g, Response
 from passlib.apps import custom_app_context as pwd_context
 import sqlite3
 import datetime
-from flask_basicauth import BasicAuth
-from functools import wraps
-
-
-def hash_password(password):
-    password_hash = pwd_context.encrypt(password)
-    return password_hash
-
-    def verify_password(password):
-        return pwd_context.verify(password, password_hash)
-
-def check_auth(username, password):
-    cur = get_db().cursor().execute("SELECT user_name, hash_pwd from users WHERE user_name=?", (username,))
-    row = cur.fetchall()
-    if row[0][0] == username and row[0][1] == password:
-        return True
-    else:
-        return False
-
-def authenticate():
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        try:
-            uid = request.authorization["username"]
-            pwd = request.authorization["password"]
-            if not uid or not pwd or check_auth(uid, pwd) == False:
-                return authenticate()
-            else:
-                return f(*args, **kwargs)
-        except:
-            return "Need authentication for this opetation\n"
-    return decorated
+from authentication import check_auth,authenticate,requires_auth
+from DatabaseInstance import get_db
 
 app = Flask(__name__)
-DATABASE = 'test_user.db'
 
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)  #create a database instance and use it for later execution
-        print("database instance is created")
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
-@app.route('/createUser', methods=['POST'])
-def createDB():
-    conn = sqlite3.connect('test_user.db')
-    c =conn.cursor()
-    conn.execute('CREATE TABLE if not exists users (user_id INTEGER PRIMARY KEY NOT NULL, user_name TEXT NOT NULL,  hash_pwd TEXT NOT NULL, name TEXT NOT NULL, email_id TEXT NOT NULL,  date_created DATE NOT NULL, is_active INTEGER NOT NULL)')
 
 #remaining handle sql query fail and return the status codes
 #create user other
@@ -75,14 +16,17 @@ def InsertUser():
     if request.method == 'POST':
         executionState:bool = False
         cur = get_db().cursor()
+        data =request.get_json(force= True)
         try:
-            data =request.get_json(force= True)
-            tmod = datetime.now()
+            date_created = datetime.datetime.now()
             is_active = 1
-            cur.execute("INSERT INTO users (user_name, hash_pwd, name, email_id, date_created, is_active ) VALUES (?, ?, ?, ?, ?, ?)",(data['user_name'], hash_password(data['hash_pwd']),data['name'],data['email_id'], tmod, is_active))
+            hash_password = pwd_context.encrypt(data['hashed_password'])
+            cur.execute( "INSERT INTO users ( user_name, hashed_password, full_name, email_id, date_created, is_active ) VALUES (:user_name, :hashed_password, :full_name, :email_id, :date_created, :is_active)",
+            {"user_name":data['user_name'], "hashed_password":hash_password, "full_name":data['full_name'], "email_id":data['email_id'], "date_created":date_created,"is_active":is_active})
             if(cur.rowcount >=1):
                 executionState = True
             get_db().commit()
+
         except:
             get_db().rollback()
             print("Error")
@@ -91,9 +35,6 @@ def InsertUser():
                 return jsonify(message="Data Instersted Sucessfully"), 200
             else:
                 return jsonify(message="Failed to insert data"), 409
-
-
-
 
 #update user
 
@@ -107,7 +48,8 @@ def UpdateUser():
             data  = request.get_json(force=True)
             uid = request.authorization["username"]
             pwd = request.authorization["password"]
-            cur.execute("UPDATE users SET hash_pwd=? WHERE user_name=? AND EXISTS(SELECT 1 FROM users WHERE user_name=? AND is_active=1)", (data['hash_pwd'], data['user_name'],data['user_name']))
+            hash_password = pwd_context.encrypt(data['hashed_password'])
+            cur.execute("UPDATE users SET hashed_password=? WHERE user_name=? AND EXISTS(SELECT 1 FROM users WHERE user_name=? AND is_active=1)", (hash_password, uid,uid))
             if(cur.rowcount >=1):
                 executionState = True
                 get_db().commit()
@@ -132,7 +74,7 @@ def DeleteUser():
         try:
             uid = request.authorization["username"]
             pwd = request.authorization["password"]
-            cur.execute("UPDATE users SET is_active =? WHERE user_name=? ", (0,uid))
+            cur.execute("UPDATE users SET is_active =? WHERE user_name=? AND EXISTS(SELECT 1 FROM users WHERE user_name=? AND is_active=1)", (0,uid,uid))
 
             if cur.rowcount >= 1:
                 executionState = True
