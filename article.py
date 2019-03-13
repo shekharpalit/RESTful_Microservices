687ab62d5e9800d7f1d4e385dc1e05b3f2669b05
rohanbedarkar1 <rohan_bedarkar@csu.fullerton.edu>
2019-03-13 16:41:00 -0700

    added db instances in article

--- article.py
+++ article.py
@@ -5,19 +5,18 @@ from flask import g
 import sqlite3
 import datetime
 import datetime
-
+from DatabaseInstance import get_db()
 
 app = Flask(__name__)
 
 
 
-@app.route('/create')
-def tcreate():
-    conn=sqlite3.connect('art.db')
-    c=conn.cursor()
-    c.execute('''Create table article(article_id integer PRIMARY KEY,title text,author text,content text,date_created text,date_modified text,isActiveArticle  integer,url text)''')
+@app.route('/createArticle',methods = ['GET'])
+def createArticleTable():
+    c=get_db().cursor()
+    c.execute('''Create table if not exists article(article_id integer PRIMARY KEY,title text,author text,content text,date_created text,date_modified text,isActiveArticle  integer,url text)''')
     conn.commit()
-    return "table created"
+    return "table created",201
 
 #insert articles
 @app.route('/article',methods = ['POST'])
@@ -26,20 +25,19 @@ def insertarticle():
         data = request.get_json(force = True)
         executionState:bool = False
         try:
-
-            with sqlite3.connect('art.db') as conn:
-                cur = conn.cursor()
-                current_time= datetime.datetime.now()
-                isActiveArticle=1
-                cur.execute("INSERT INTO article(title,author,date_created,date_modified,isActiveArticle,url) VALUES (:title,:author,:date_created,:date_modified,:isActiveArticle,:url)",{"title":data['title'],"author":data['author'],"date_created":current_time,"date_modified":current_time,"isActiveArticle":isActiveArticle,"url":data['url']})
-                last_inserted_row = cur.lastrowid
-                url_article=("http://127.0.0.1:5000/article/"+str(last_inserted_row)+"")
-                cur.execute("UPDATE article set url=? where article_id=?",(url_article,last_inserted_row))
-                if(cur.rowcount >=1):
+            cur = get_db().cursor()
+            current_time= datetime.datetime.now()
+            isActiveArticle=1
+            cur.execute("INSERT INTO article(title,author,date_created,date_modified,isActiveArticle,url) VALUES (:title,:author,:date_created,:date_modified,:isActiveArticle,:url)",{"title":data['title'],"author":data['author'],"date_created":current_time,"date_modified":current_time,"isActiveArticle":isActiveArticle,"url":data['url']})
+            last_inserted_row = cur.lastrowid
+            url_article=("http://127.0.0.1:5000/article/"+str(last_inserted_row)+"")
+            cur.execute("UPDATE article set url=? where article_id=?",(url_article,last_inserted_row))
+            if(cur.rowcount >=1):
                     executionState = True
-                    conn.commit()
+                    get_db().commit()
         except:
-            print(Error)
+            get_db().rollback()
+            print("Error")
         finally:
             if executionState:
                 return jsonify(message="Data Instersted Sucessfully"), 200
@@ -54,35 +52,29 @@ def insertarticle():
 def latestArticle():
     if request.method == 'GET':
         data = request.args.get('limit')
-        data1 = request.args.get('number')
-        with sqlite3.connect('art.db') as conn:
-            if data is not None :
-                cur = conn.cursor()
-                cur.execute("select * from article  where isActiveArticle = 1 order by date_created desc limit :data",  {"data":data})
-                row = cur.fetchall()
-                conn.commit()
-                return jsonify(row)
+        if data is not None :
+            cur = get_db().cursor()
+            cur.execute("select * from article  where isActiveArticle = 1 order by date_created desc limit :data",  {"data":data})
+            row = cur.fetchall()
+            return jsonify(row)
 
         #limit = request.args.get('limit')
-            if data is None and data1 is None:
-                cur = conn.cursor()
-                cur.execute('''Select * from article''')
-                #cur.execute("select art,time_created from post_article order by time_created desc limit "+data)
-                row = cur.fetchall()
-                conn.commit()
-                return jsonify(row)
+        if data is None:
+            cur = get_db().cursor()
+            cur.execute('''Select * from article''')
+            row = cur.fetchall()
+            return jsonify(row)
 
 #get article from url...article id needed
 @app.route('/article/<string:art_id>',methods = ['GET'])
 def getTagsFromArticle(art_id):
     if request.method == 'GET':
-        with sqlite3.connect('art.db') as conn:
-            cur = conn.cursor()
-            article_id=art_id
-            sql=("SELECT * from  article WHERE article_id="+article_id)
-            cur.execute(sql)
-            row = cur.fetchall()
-            return jsonify(row)
+        cur = get_db().cursor()
+        article_id=art_id
+        sql=("SELECT * from  article WHERE article_id="+article_id)
+        cur.execute(sql)
+        row = cur.fetchall()
+        return jsonify(row)
 
 # get single article by name.....multiple url /article same get method
 
@@ -90,13 +82,11 @@ def getTagsFromArticle(art_id):
 def Article():
     if request.method == 'GET':
         data = request.args.get('title')
-        print(data)
-        with sqlite3.connect('art.db') as conn:
-            cur = conn.cursor()
-            cur.execute("select * from article where title like :data ", {"data":data})
-            row = cur.fetchall()
-            conn.commit()
-            return jsonify(row)
+        cur = get_db().cursor()
+        cur.execute("select * from article where title like :data ", {"data":data})
+        row = cur.fetchall()
+        conn.commit()
+        return jsonify(row),200
 
 
 # update article
@@ -107,15 +97,14 @@ def updateArticle():
         executionState:bool = False
         try:
             data = request.get_json(force = True)
-            with sqlite3.connect('art.db') as conn:
-                cur = conn.cursor()
-                tmod= datetime.datetime.now()
-                cur.execute("UPDATE article set content=?,date_modified=? where article_id=?", (data['content'],tmod,data['article_id']))
-                if(cur.rowcount >=1):
-                    executionState = True
-                    conn.commit()
+            cur = get_db().cursor()
+            tmod= datetime.datetime.now()
+            cur.execute("UPDATE article set content=?,date_modified=? where article_id=?", (data['content'],tmod,data['article_id']))
+            if(cur.rowcount >=1):
+                executionState = True
+                    get_db().commit()
         except:
-            conn.rollback()
+            get_db().rollback()
             print("Error in update")
         finally:
             if executionState:
@@ -131,18 +120,16 @@ def deleteArticle():
         try:
             article_id = request.args.get('article_id')
             executionState:bool = False
-            with sqlite3.connect('art.db') as conn:
-                cur = conn.cursor()
-                cur.execute("delete from article where article_id=:article_id",{"article_id":article_id})
-                row = cur.fetchall()
-                if cur.rowcount >= 1:
-                    executionState = True
-                conn.commit()
+            cur = get_db().cursor()
+            cur.execute("delete from article where article_id=:article_id",{"article_id":article_id})
+            row = cur.fetchall()
+            if cur.rowcount >= 1:
+                executionState = True
+            get_db().commit()
 
-        except sqlite3.Error as er:
-            conn.rollback()
-            print("Error:")
-            print(er.message)
+        except:
+            get_db().rollback()
+            print("Error")
         finally:
             if executionState:
                 return jsonify(message="Deleted Article SucessFully"),200
